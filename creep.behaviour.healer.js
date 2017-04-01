770d126a39a911a95f2f966af5ab50dbf2f0fdff
Thom Allen <thomwork@gmail.com>
2017-04-01 02:04:30 +0100

    Update healer behaviour for following etc

--- creep.behaviour.healer.js
+++ creep.behaviour.healer.js
@@ -5,8 +5,14 @@ mod.run = function(creep) {
       creep.flee = ((creep.flee) || (!creep.hasActiveBodyparts([HEAL])) || (creep.data.destiny.squad && creep.missingBodyparts() > 5));
     // Assign next Action
     let oldTargetId = creep.data.targetId;
-    if( creep.action == null || ['guarding','idle'].includes(creep.action.name)) {
-        this.nextAction(creep);
+    if (!creep.action || creep.action.name === 'idle' ||
+        (creep.action.name === 'guarding' &&
+            (!creep.flag || creep.flag.pos.roomName === creep.pos.roomName || creep.leaveBorder())
+        )
+    ) {
+        if( creep.data.destiny && creep.data.destiny.task && Task[creep.data.destiny.task] && Task[creep.data.destiny.task].nextAction ) 
+            Task[creep.data.destiny.task].nextAction(creep);
+        else this.nextAction(creep);
     }
     
     // Do some work
@@ -18,8 +24,8 @@ mod.run = function(creep) {
 };
 mod.actions = (creep) => {
     return [
-      
       Creep.action.healing,
+      Creep.action.following,
       Creep.action.invading,
       Creep.action.boosting,
       Creep.action.staging,
